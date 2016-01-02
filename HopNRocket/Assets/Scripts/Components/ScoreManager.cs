e1b1098e9cc1f55d8a497708d78193aaad724bb8
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-01 22:44:31 -0500

    - Added a persistent score manager to keep track of score across retries and added some HUD elements.

---
+++ HopNRocket/Assets/Scripts/Components/ScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager
+: MonoBehaviour
+{
+	private static ScoreManager s_Instance = null;
+
+	public static ScoreManager instance
+	{
+		get{ return s_Instance; }
+	}
+
+	private uint m_BestScore = 0;
+	private uint m_CurrentScore = 0;
+
+	void Awake()
+	{
+		//-- Apply singleton logic upon object creation; only one
+		//   score manager should persist per game session.
+		if( null == s_Instance )
+		{
+			s_Instance = this;
+			DontDestroyOnLoad( gameObject );
+		}
+		else
+		{
+			Destroy( gameObject );
+		}
+	}
+
+	void OnLevelWasLoaded()
+	{
+		print ( this.GetInstanceID() );
+	}
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+	
+	}
+}
