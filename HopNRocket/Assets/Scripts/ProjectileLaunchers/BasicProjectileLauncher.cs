c3def2afbf6dd7b0c7a67d30fbfea723abadb087
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-03 18:01:04 -0500

    - Improved projectile launcher code structure and added some projectile launchers
    A non-abstract default projectile launcher class was created which takes
    care of the most common configurations for projectile launchers. Two
    projectile launchers were created for the player for the jump shot and
    the forward shot. The existing BasicProjectileLauncher was modified to
    subclass DefaultProjectileLauncher.

--- HopNRocket/Assets/Scripts/ProjectileLaunchers/BasicProjectileLauncher.cs
+++ HopNRocket/Assets/Scripts/ProjectileLaunchers/BasicProjectileLauncher.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class BasicProjectileLauncher
-: ProjectileLauncher
+: DefaultProjectileLauncher
 {
 	public float m_LaunchDelay;
 	public float m_LaunchInterval;
-	public float m_LaunchForce;
 
 	void Start()
 	{
@@ -23,29 +22,5 @@ public class BasicProjectileLauncher
 
 			yield return new WaitForSeconds( m_LaunchInterval );
 		}
-	}
-
-	protected override ProjectileLaunchEvent GenerateLaunchEvent()
-	{
-		//-- Resolve the direction of the shot relavitve to the world
-		Transform objectTransform = gameObject.transform;
-		Vector3 direction = new Vector3( 1.0f, 0.0f, 0.0f );
-		while( null != objectTransform )
-		{
-			//-- Transform the direction by the transform
-			direction = objectTransform.TransformVector( direction );
-
-			//-- Get the parent game object
-			objectTransform = objectTransform.parent;
-		}
-
-		//-- Generate the launch event
-		ProjectileLaunchEvent eventInfo = new ProjectileLaunchEvent();
-		{
-			eventInfo.launchForce = m_LaunchForce;
-			eventInfo.launchDirection = new Vector2( direction.x, direction.y );
-		}
-
-		return eventInfo;
 	}
 }
---
+++ HopNRocket/Assets/Scripts/ProjectileLaunchers/DefaultProjectileLauncher.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultProjectileLauncher
+: ProjectileLauncher
+{
+	public float m_LaunchForce;
+
+	protected void ConfigureDefaultLaunchEvent( ProjectileLaunchEvent launchEvent )
+	{
+		//-- Does nothing by default; meant to be overridden by subclasses
+		//   if they need the launch event to diverge from default settings
+		//   while still benefiting from the generation of a default launch
+		//   event.
+	}
+
+	protected sealed override ProjectileLaunchEvent GenerateLaunchEvent()
+	{
+		//-- Resolve the direction of the shot relavitve to the world
+		Transform objectTransform = gameObject.transform;
+		Vector3 direction = new Vector3( 1.0f, 0.0f, 0.0f );
+		while( null != objectTransform )
+		{
+			//-- Transform the direction by the transform
+			direction = objectTransform.TransformDirection( direction );
+
+			//-- Get the parent game object
+			objectTransform = objectTransform.parent;
+		}
+
+		//-- Generate the launch event
+		ProjectileLaunchEvent defaultEventInfo = new ProjectileLaunchEvent();
+		{
+			defaultEventInfo.launchForce = m_LaunchForce;
+			defaultEventInfo.launchDirection = new Vector2( direction.x, direction.y );
+		}
+
+		//-- Give the subclass (if any) a chance to modify the default launch event
+		ConfigureDefaultLaunchEvent( defaultEventInfo );
+
+		return defaultEventInfo;
+	}
+}
