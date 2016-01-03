c184c17e1792f30e275aa4ccc7997cb090b45932
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-03 13:36:36 -0500

    - Disabled destroy on contact with parent objects
    Added a mechanism to prevent projectiles and such from destroying
    themselves when they come into contact with a game object included in
    their parent hierarchy.

--- HopNRocket/Assets/Scripts/DestroyOnContact.cs
+++ HopNRocket/Assets/Scripts/DestroyOnContact.cs
@@ -6,8 +6,8 @@ public class DestroyOnContact
 {
 	void OnCollisionEnter2D( Collision2D collision )
 	{
-		//-- Destroy this game object if it bumps into something solid
-		if( !collision.collider.isTrigger )
+		//-- Destroy this game object if it bumps into something solid that isn't its parent
+		if( !collision.collider.isTrigger && !IsParent( collision.gameObject ) )
 		{
 			Destroy( gameObject );
 		}
@@ -15,10 +15,26 @@ public class DestroyOnContact
 
 	void OnTriggerEnter2D( Collider2D collider )
 	{
-		//-- Destroy this game object if it bumps into something solid
-		if( !collider.isTrigger )
+		//-- Destroy this game object if it bumps into something solid that isn't its parent
+		if( !collider.isTrigger && !IsParent( collider.gameObject ) )
 		{
 			Destroy( gameObject );
 		}
+	}
+
+	bool IsParent( GameObject otherGameObject )
+	{
+		Transform xform = gameObject.transform;
+		while( null != xform )
+		{
+			if( xform == otherGameObject.transform )
+			{
+				return true;
+			}
+
+			xform = xform.parent;
+		}
+
+		return false;
 	}
 }
