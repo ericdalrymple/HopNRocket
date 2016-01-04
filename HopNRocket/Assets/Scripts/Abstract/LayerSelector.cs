95c38188b4856d67f65ea87675931897c7891e43
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 04:58:23 -0500

    - Added a handy super script for scripts that need the dev to specify a list of layers

---
+++ HopNRocket/Assets/Scripts/Abstract/LayerSelector.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayerSelector
+: MonoBehaviour
+{
+	public string[] m_TargetLayerNames;
+	
+	private int[] m_TargetLayerIndices;
+	
+	void Awake()
+	{
+		//-- Cache the indices for all of our target layers
+		int targetLayerCount = m_TargetLayerNames.Length;
+		
+		m_TargetLayerIndices = new int[targetLayerCount];
+		for( int i = 0; i < targetLayerCount; ++i )
+		{
+			m_TargetLayerIndices[i] = LayerMask.NameToLayer( m_TargetLayerNames[i] );
+		}
+	}
+
+	protected bool IsOnTargetLayer( GameObject gameObject )
+	{
+		foreach( int layer in m_TargetLayerIndices )
+		{
+			if( layer == gameObject.layer )
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
