a11d86342864d35b265fc52843034a0bd39d97e9
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 14:48:30 -0500

    - Added a state behaviour for affecting the game's scroll speed

---
+++ HopNRocket/Assets/Scripts/Behaviours/AffectScrollSpeedBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[SharedBetweenAnimators]
+public class AffectScrollSpeedBehaviour
+: StateMachineBehaviour
+{
+	public enum ScrollSpeedDirection
+	{
+		  INCREMENT = 0
+		, DECREMENT
+	}
+
+	[Tooltip( "Whether this behaviour increments or decrements the game's scroll speed" )]
+	public ScrollSpeedDirection m_Mode;
+
+	[Tooltip( "Number of times to increment or decrement the game scroll speed" )]
+	public int m_Count = 1;
+
+	public override void OnStateEnter( Animator animator, AnimatorStateInfo stateInfo, int layerIndex )
+	{
+		string message = "";
+
+		switch( m_Mode )
+		{
+		case ScrollSpeedDirection.INCREMENT:
+		{
+			message = "IncrementScroolSpeed";
+			break;
+		}
+
+		case ScrollSpeedDirection.DECREMENT:
+		{
+			message = "DecrementScrollSpeed";
+			break;
+		}
+		}
+
+		if( 0 < message.Length )
+		{
+			for( int i = 0; i < m_Count; ++i )
+			{
+				GameController.instance.gameObject.BroadcastMessage( message );
+			}
+		}
+	}
+}
