811901cb0043c43d9ad7ba3e4ad16a8c2c6dbbe6
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 05:06:35 -0500

    - Implemented simpler, more manageable/expandable damage system

---
+++ HopNRocket/Assets/Scripts/DamageOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnCollision
+: LayerSelector
+{
+	private static readonly string MESSAGE_HIT = "OnHit";
+
+	void OnCollisionEnter2D( Collision2D collision )
+	{
+		if( IsOnTargetLayer( collision.gameObject ) )
+		{
+			SendHit( collision.gameObject );
+		}
+	}
+
+	void SendHit( GameObject gameObject )
+	{
+		gameObject.BroadcastMessage( MESSAGE_HIT, SendMessageOptions.DontRequireReceiver );
+	}
+}
---
+++ HopNRocket/Assets/Scripts/DamageOnContact.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnContact
+: LayerSelector
+{
+	private static readonly string MESSAGE_HIT = "OnHit";
+
+	void OnCollisionEnter2D( Collision2D collision )
+	{
+		if( IsOnTargetLayer( collision.gameObject ) )
+		{
+			SendHit( collision.gameObject );
+		}
+	}
+
+	void OnTriggerEnter2D( Collider2D collider )
+	{
+		if( IsOnTargetLayer( collider.gameObject ) )
+		{
+			SendHit( collider.gameObject );
+		}
+	}
+
+	void SendHit( GameObject gameObject )
+	{
+		gameObject.BroadcastMessage( MESSAGE_HIT, SendMessageOptions.DontRequireReceiver );
+	}
+}
