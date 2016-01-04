db6f96d98cc5ca6fce8396ffab313726a8038af1
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 14:51:06 -0500

    - Added doc to world scroller and added better hooks for controlling the game scroll speed

--- HopNRocket/Assets/Scripts/WorldScroller.cs
+++ HopNRocket/Assets/Scripts/WorldScroller.cs
@@ -8,8 +8,15 @@ public class WorldScroller
 	private static readonly string SCROLLABLE_TAG = "Scrollable";
 
 	//-- Memeber variables
+	[Tooltip( "Scroll speed, in units per second, at which the game initially scrolls" )]
 	public float m_InitialScrollSpeed;
+
+	[Tooltip( "Rate, in units per second per second, at which the game's scroll speed " +
+			  "increases over time" )]
 	public float m_ScrollAcceleration;
+
+	[Tooltip( "Discrete amount, in units per second, by which the game's scroll speed " +
+		      "changes when using WorldScroller's increment/decrement methods" )]
 	public float m_ScrollSpeedIncrement;
 
 	private float m_CurrentScrollSpeed;
@@ -55,18 +62,21 @@ public class WorldScroller
 		}
 	}
 
-	void OnPlayerJump()
+	void DecrementScrollSpeed()
 	{
-		//-- Speed up the game on every hop
-		m_CurrentScrollSpeed += m_ScrollSpeedIncrement;
-	}
-
-	void OnPlayerShot()
-	{
-		//-- Slow down the game on every shot
+		//-- Slow down the game
 		m_CurrentScrollSpeed -= m_ScrollSpeedIncrement;
 
 		//-- Can't slow down more than the initial scroll speed
+		m_CurrentScrollSpeed = Mathf.Max( m_InitialScrollSpeed, m_CurrentScrollSpeed );
+	}
+
+	void IncrementScroolSpeed()
+	{
+		//-- Speed up the game
+		m_CurrentScrollSpeed += m_ScrollSpeedIncrement;
+
+		//-- Can't slow down more than the initial scroll speed (handles negative scroll speed increments)
 		m_CurrentScrollSpeed = Mathf.Max( m_InitialScrollSpeed, m_CurrentScrollSpeed );
 	}
 }
