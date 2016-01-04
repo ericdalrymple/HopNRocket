a65335d1e7c7954306a96460f91f74a25008a17f
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 05:00:47 -0500

    - Added a script that resizes a sibling circle collider relative to the game area height
    Generalized the former turret sensor auto sizer.

---
+++ HopNRocket/Assets/Scripts/AutoSizeCollider.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSizeCollider
+: MonoBehaviour
+{
+	public float m_ScreenHeightRatio;
+
+	private CircleCollider2D m_Collider;
+
+	void Start()
+	{
+		GameObject gameAreaObject = GameObject.FindGameObjectWithTag( "GameArea" );
+		if( null != gameAreaObject )
+		{
+			BoxCollider2D gameArea = gameAreaObject.GetComponent<BoxCollider2D>();
+
+			m_Collider = GetComponent<CircleCollider2D>();
+			if( null != m_Collider )
+			{
+				m_Collider.radius = gameArea.size.y * m_ScreenHeightRatio;
+			}
+		}
+	}
+}
