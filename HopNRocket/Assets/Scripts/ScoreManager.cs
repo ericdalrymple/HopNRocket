c6549ebf7f3e12684b8231705b1eec4ce0de8e7b
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 14:50:12 -0500

    - Player now earns points for staying alive

--- HopNRocket/Assets/Scripts/ScoreManager.cs
+++ HopNRocket/Assets/Scripts/ScoreManager.cs
@@ -11,6 +11,15 @@ public class ScoreManager
 		get{ return s_Instance; }
 	}
 
+	[Tooltip( "Interval, in seconds, at which points are awarded automatically" )]
+	public float m_TimerPointInterval = 1.0f;
+
+	[Tooltip( "Number of points to award on every timer point interval" )]
+	public float m_TimerPoints = 25.0f;
+
+	private float m_BestScore = 0.0f;
+	private float m_TimerScore = 0.0f;
+
 	void Awake()
 	{
 		//-- Apply singleton logic upon object creation; only one
@@ -18,26 +27,48 @@ public class ScoreManager
 		if( null == s_Instance )
 		{
 			s_Instance = this;
-			DontDestroyOnLoad( gameObject );
-		}
-		else
-		{
-			Destroy( gameObject );
 		}
 	}
 
 	void OnLevelWasLoaded()
 	{
-
+		m_TimerScore = 0.0f;
 	}
 
-	void Start()
+	public int GetBestScore()
 	{
-
+		return (int)m_BestScore;
 	}
 
-	void Update()
+	public int GetTotalScore()
 	{
-	
+		return (int)m_TimerScore;
+	}
+
+	void OnGameStateChange( GameController.GameStateEvent eventInfo )
+	{
+		if( GameController.instance.IsGamePlaying() )
+		{
+			StartCoroutine( AddTimerPoints() );
+		}
+		else if( GameController.GameState.GAME_OVER == eventInfo.currentState )
+		{
+			int oldBestScore = GetBestScore();
+			int sessionScore = GetTotalScore();
+			if( oldBestScore < sessionScore )
+			{
+				m_BestScore = sessionScore;
+			}
+		}
+	}
+
+	IEnumerator AddTimerPoints()
+	{
+		do
+		{
+			yield return new WaitForSeconds( m_TimerPointInterval );
+			m_TimerScore += m_TimerPoints;
+		}
+		while( GameController.instance.IsGamePlaying() );
 	}
 }
