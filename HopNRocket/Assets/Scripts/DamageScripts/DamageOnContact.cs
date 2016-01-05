8933f63b573af86c9ccab2967b6725e9094a6173
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-05 17:45:50 -0500

    - Consolidated damage scripts

--- HopNRocket/Assets/Scripts/DamageScripts/DamageOnContact.cs
+++ HopNRocket/Assets/Scripts/DamageScripts/DamageOnContact.cs
@@ -4,8 +4,16 @@ using System.Collections;
 public class DamageOnContact
 : LayerSelector
 {
+	public bool m_OnCollision = true;
+	public bool m_OnTrigger = true;
+
 	void OnCollisionEnter2D( Collision2D collision )
 	{
+		if( !m_OnCollision )
+		{
+			return;
+		}
+
 		if( IsOnTargetLayer( collision.gameObject ) )
 		{
 			SendHit( collision.gameObject );
@@ -14,6 +22,11 @@ public class DamageOnContact
 
 	void OnTriggerEnter2D( Collider2D collider )
 	{
+		if( !m_OnTrigger )
+		{
+			return;
+		}
+
 		if( IsOnTargetLayer( collider.gameObject ) )
 		{
 			SendHit( collider.gameObject );
@@ -22,6 +35,16 @@ public class DamageOnContact
 
 	void SendHit( GameObject gameObject )
 	{
-		gameObject.BroadcastMessage( Killable.MESSAGE_HIT, SendMessageOptions.DontRequireReceiver );
+		GameObject messageTarget = gameObject;
+
+		Transform parent = gameObject.transform.parent;
+		if( (null != parent) && IsOnTargetLayer( parent.gameObject ) )
+		{
+			//-- The object that was collided with is most likely a collidable
+			//   child of the game object wee need to hit
+			messageTarget = parent.gameObject;
+		}
+
+		messageTarget.BroadcastMessage( Killable.MESSAGE_HIT, SendMessageOptions.DontRequireReceiver );
 	}
 }
