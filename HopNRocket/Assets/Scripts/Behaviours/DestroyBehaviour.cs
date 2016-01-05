b0e3cae78130d1fa9d55c6a49b844b21cb0826e8
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-05 17:47:52 -0500

    - Better explosions and more control when destroying objects from animator state

--- HopNRocket/Assets/Scripts/Behaviours/DestroyBehaviour.cs
+++ HopNRocket/Assets/Scripts/Behaviours/DestroyBehaviour.cs
@@ -5,22 +5,34 @@ using System.Collections;
 public class DestroyBehaviour
 : StateMachineBehaviour
 {
+	public bool m_DestroyParent = false;
+
 	public GameObject m_Explosion = null;
+	public bool m_ScaleExplosion = true;
 
 	override public void OnStateEnter( Animator animator, AnimatorStateInfo stateInfo, int layerIndex )
 	{
+		Transform animatorTransform = animator.transform;
+		Transform targetTransform = m_DestroyParent? animatorTransform.parent : animatorTransform;
+
 		if( null != m_Explosion )
 		{
 			GameObject explosion = Instantiate( m_Explosion
-			           						  , animator.gameObject.transform.position
+			                                  , targetTransform.position
 			           						  , Quaternion.identity ) as GameObject;
 
 			//-- Transform the explosion to its subject just in case it looks directional
-			explosion.transform.localScale = animator.transform.localScale;
-			explosion.transform.position = animator.transform.position;
-			explosion.transform.rotation = animator.transform.rotation;
+			explosion.transform.rotation = Quaternion.RotateTowards( explosion.transform.rotation
+			                                                       , targetTransform.rotation
+			                                                       , 360.0f );
+
+			if( m_ScaleExplosion )
+			{
+				explosion.transform.localScale = Vector3.Scale( explosion.transform.localScale
+				                                              , targetTransform.localScale );
+			}
 		}
 
-		Destroy( animator.gameObject );
+		Destroy( targetTransform.gameObject );
 	}
 }
