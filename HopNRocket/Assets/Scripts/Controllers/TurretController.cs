a821c551df37589d657ae40a7cfcaea282fc1e33
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 23:59:08 -0500

    - Stripped damage logic from turret, now uses Killable

--- HopNRocket/Assets/Scripts/Controllers/TurretController.cs
+++ HopNRocket/Assets/Scripts/Controllers/TurretController.cs
@@ -4,22 +4,16 @@ using System.Collections;
 public class TurretController
 : MonoBehaviour
 {
-	public int m_MaxHP;
 	public int m_Score;
 
 	//-- Animator parameter hashes
 	private static readonly int m_HashPlayerDetected = Animator.StringToHash( "PlayerDetected" );
-	private static readonly int m_HashRemainingHits  = Animator.StringToHash( "RemainingHits" );
 
-	private int m_HP;
 	private Animator m_Animator;
 
 	void Start()
 	{
 		m_Animator = GetComponent<Animator>();
-
-		//-- Initialize HP
-		SetHP( m_MaxHP );
 	}
 
 	void Update()
@@ -46,23 +40,6 @@ public class TurretController
 			{
 				m_Animator.SetBool( m_HashPlayerDetected, false );
 			}
-		}
-	}
-
-	void OnHit()
-	{
-		SetHP( m_HP - 1 );
-	}
-
-	void SetHP( int newHP )
-	{
-		//-- Update HP to new value
-		m_HP = Mathf.Max( 0, newHP );
-
-		//-- Synch the value to the animator
-		if( null != m_Animator )
-		{
-			m_Animator.SetInteger( m_HashRemainingHits, m_HP );
 		}
 	}
 }
