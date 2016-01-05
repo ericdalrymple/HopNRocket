1b2b3505d6f53796f9c117d54565ed5bb7e069f6
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 23:51:07 -0500

    - Added a util to make Singleton game objects

---
+++ HopNRocket/Assets/Scripts/Abstract/SingletonObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Superclass for game object controller scripts that need to
+ * persist across levels and only have one active instance.
+ */
+public class SingletonObject<T>
+: MonoBehaviour
+where T : SingletonObject<T>
+{
+	private static SingletonObject<T> s_Instance = null;
+	
+	public static T instance
+	{
+		get
+		{
+			return (T)s_Instance;
+		}
+	}
+	
+	void OnEnable()
+	{
+		if( null == instance )
+		{
+			s_Instance = this;
+			DontDestroyOnLoad( gameObject );
+		}
+		else
+		{
+			Destroy( gameObject );
+		}
+	}
+}
