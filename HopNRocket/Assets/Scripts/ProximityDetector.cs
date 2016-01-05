55ae3074328ced55c5b12feedfb407fa8bf57ea7
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-05 17:28:51 -0500

    - Implemented a proximity detector that can alert certain objects when triggered

---
+++ HopNRocket/Assets/Scripts/ProximityDetector.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProximityDetector
+: LayerSelector
+{
+	public enum NotificationMode
+	{
+		  SELF = 0
+		, SELF_AND_CHILDREN
+		, PARENT
+		, PARENT_AND_SIBLINGS
+	}
+
+	private static readonly string MESSAGE_PROXIMITY_ENTER = "OnProximityEnter";
+	private static readonly string MESSAGE_PROXIMITY_EXIT = "OnProximityExit";
+
+	public NotificationMode m_NotificationMode;
+	public bool m_RequireReceiver;
+
+	void OnTriggerEnter2D( Collider2D collider )
+	{
+		if( base.IsOnTargetLayer( collider.gameObject ) )
+		{
+			Notify( MESSAGE_PROXIMITY_ENTER, collider.gameObject );
+		}
+	}
+
+	void OnTriggerExit2D( Collider2D collider )
+	{
+		if( base.IsOnTargetLayer( collider.gameObject ) )
+		{
+			Notify( MESSAGE_PROXIMITY_EXIT, collider.gameObject );
+		}
+	}
+
+	void Notify( string message, GameObject detectedObject )
+	{
+		SendMessageOptions options = m_RequireReceiver? SendMessageOptions.RequireReceiver : SendMessageOptions.DontRequireReceiver;
+
+		switch( m_NotificationMode )
+		{
+			case NotificationMode.SELF:
+			{
+				gameObject.SendMessage( message, detectedObject, options );
+				break;
+			}
+
+			case NotificationMode.SELF_AND_CHILDREN:
+			{
+				gameObject.BroadcastMessage( message, detectedObject, options );
+				break;
+			}
+
+			case NotificationMode.PARENT:
+			{
+				if( null != transform.parent )
+				{
+					transform.parent.gameObject.SendMessage( message, detectedObject, options );
+				}
+
+				break;
+			}
+
+			case NotificationMode.PARENT_AND_SIBLINGS:
+			{
+				if( null != transform.parent )
+				{
+					transform.parent.gameObject.BroadcastMessage( message, detectedObject, options );
+				}
+				
+				break;
+			}
+		}
+	}
+}
