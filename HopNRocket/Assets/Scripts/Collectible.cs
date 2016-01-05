7dbc2b127804b58919a217c4433c888c4afee8fe
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-04 19:18:25 -0500

    - Implemented collectibles

---
+++ HopNRocket/Assets/Scripts/Collectible.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible
+: MonoBehaviour
+{
+	void OnTriggerEnter2D( Collider2D collider )
+	{
+		if( collider.gameObject.CompareTag( "Player" ) )
+		{
+			InventoryManager.instance.AddItems( 1 );
+			Destroy( gameObject );
+		}
+	}
+}
