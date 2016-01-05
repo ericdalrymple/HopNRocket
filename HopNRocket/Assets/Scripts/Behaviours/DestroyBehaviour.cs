971a3b4eba26737795cedecaee9c7240d1ddd767
ericdalrymple <eric.dalrymple@gmail.com>
2016-01-05 07:33:41 -0500

    - Added explosions

--- HopNRocket/Assets/Scripts/Behaviours/DestroyBehaviour.cs
+++ HopNRocket/Assets/Scripts/Behaviours/DestroyBehaviour.cs
@@ -1,12 +1,26 @@
-﻿using UnityEngine;
+﻿ using UnityEngine;
 using System.Collections;
 
 [SharedBetweenAnimators]
 public class DestroyBehaviour
 : StateMachineBehaviour
 {
-	override public void OnStateExit( Animator animator, AnimatorStateInfo stateInfo, int layerIndex )
+	public GameObject m_Explosion = null;
+
+	override public void OnStateEnter( Animator animator, AnimatorStateInfo stateInfo, int layerIndex )
 	{
+		if( null != m_Explosion )
+		{
+			GameObject explosion = Instantiate( m_Explosion
+			           						  , animator.gameObject.transform.position
+			           						  , Quaternion.identity ) as GameObject;
+
+			//-- Transform the explosion to its subject just in case it looks directional
+			explosion.transform.localScale = animator.transform.localScale;
+			explosion.transform.position = animator.transform.position;
+			explosion.transform.rotation = animator.transform.rotation;
+		}
+
 		Destroy( animator.gameObject );
 	}
 }
